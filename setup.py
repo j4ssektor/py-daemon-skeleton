2dcb80518a0c579cc760f9d37859f9203a3ccb43
Nikolay Nozhenko <n.nozhenko@i-free.com>
2016-01-23 21:17:14 +0300

    add requirements for deb package

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ setup(
     name='skeleton',
     version='0.1.0',
     packages=find_packages(),
-    scripts=['bin/run',],
     author='Nikolai Nozhenko',
     author_email='j4ssektor@gmail.com',
     license = 'MIT',
     description='',
-    scripts=['py-daemon-skeleton'],
+    scripts=['bin/py-daemon-skeleton']
 )
